93541f702ba070b5a2275e584752ff055b96df74
Bhargav Gohil <bhargavgohil25@gmail.com>
2020-10-09 01:40:48 +0530

    Added Grid and perfectly Uploads the images

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -9,7 +9,11 @@ const ImageGrid = () => {
 
     return(
         <div className = "img-grid">
-            images
+            { docs && docs.map(doc => (
+                <div className = "img-wrap" key={doc.id}>
+                    <img src = {doc.url} alt = "uploaded pic"/>
+                </div>
+            ))}
         </div>
     );
 }
